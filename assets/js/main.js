ce2ffd2cf6e448597aedae5a8558d91e9cb52ab0
Leo Laksmana <beol@laksmana.com>
2015-11-13 20:48:34 +0800

    Modify tracker

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,21 +1,42 @@
+window.GoogleAnalyticsObject = "__ga__";
+window.__ga__ = {
+    q: [['create', 'UA-8853012-4', 'auto']],
+    l: Date.now()
+};
+
 require.config({
     baseUrl: "assets/js",
     paths: {
         "bootstrap": "bootstrap",
         "jquery": "jquery",
-        "tracker": "tracker"
+        "ga": [
+            'https://www.google-analytics.com/analytics',
+            'http://www.google-analytics.com/analytics',
+            'analytics'
+        ],
+        "qc": [
+            "https://secure.quantserve.com/quant",
+            "http://edge.quantserve.com/quant",
+            'quant'
+        ]
     },
     shim: {
         "bootstrap": {
             deps: ["jquery"],
             export: "Bootstrap"
         },
-        "tracker": {
-            exports: "tracker"
+        "ga": {
+            exports: "__ga__"
+        },
+        "qc": {
+            exports: "qc"
         }
     }
 });
 
-require(["tracker", "jquery", "bootstrap"], function() {
-
+require(["ga", "qc", "jquery", "bootstrap"], function(ga, qc, $, Bootstrap) {
+    ga("send", "pageview");
+    qc._qevents.push({
+        qacct: "p-2hmv506hLezgG"
+    });
 });
